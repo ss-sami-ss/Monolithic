e8527ce3f744ae9bc34472a1d23130c6bf47b894
Saman Gh <so_sami_so@yahoo.com>
2020-09-08 17:57:05 +0430

    Monolithic_SinglePage_Add_Save

---
+++ Monolithic_SinglePage/Controllers/CategoryController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Monolithic_SinglePage.Models.ViewModel;
+using Monolithic_SinglePage.Models.DomainModels.DTO.EF;
+
+namespace Monolithic_SinglePage.Controllers
+{
+    public class CategoryController : Controller
+    {
+        #region [- ctor -]
+        public CategoryController()
+        {
+            Ref_CategoryViewModel = new CategoryViewModel();
+        }
+        #endregion
+
+        #region [- Props -]
+        public CategoryViewModel Ref_CategoryViewModel { get; set; }
+        #endregion
+
+        #region [- Category() -]
+        public ActionResult Category()
+        {
+            return View();
+        }
+        #endregion
+
+        #region [- Create -]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AllowAnonymous]
+        public ActionResult Create(Category ref_Category)
+        {
+            if (ModelState.IsValid)
+            {
+                return Json(new { message = "Success" }, JsonRequestBehavior.AllowGet);
+
+            }
+            else
+            {
+                return Json(new { ModelState_IsValid = "False" }, JsonRequestBehavior.AllowGet);
+            }
+        } 
+        #endregion
+    }
+}
